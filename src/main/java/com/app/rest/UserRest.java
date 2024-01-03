6f892abad27bc114776672553d33b754faf63d83
basheer-eidiko <basheerbabu.shaik@eidiko-india.com>
2024-01-03 15:49:57 +0530

    fix: cross origin issue

--- src/main/java/com/app/rest/UserRest.java
+++ src/main/java/com/app/rest/UserRest.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,7 +18,7 @@ import com.app.service.IUserService;
 
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-
+@CrossOrigin(origins = {"*","https://interviewguru.onrender.com"})
 @RestController
 @RequestMapping("/api/user")
 public class UserRest {
